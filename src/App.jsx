a2dca95dd3f5bdce4431393e4b17584e33d42ade
Dipannita_Mondal <dipannitamisti10@gmail.com>
2024-07-02 22:06:05 +0530

    Topbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { ColorModeContext,useMode } from './theme' ;
+import { CssBaseline,ThemeProvider } from '@mui/material';
+import { Route,Routes } from 'react-router-dom';
+import TopBar from './scenes/global/TopBar';
+import { Dashboard } from '@mui/icons-material';
+
+
 
 const App = () => {
-  return (
-    <div>App</div>
+
+  const [theme,colorMode] = useMode();
+
+  return (<ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+    <CssBaseline/>
+   
+    <div className='app'>
+    <main className='content'>
+      <TopBar/>
+      <Routes>
+        <Route path='/' element={<Dashboard/>}/>
+      </Routes>
+    </main>
+    </div>
+    </ThemeProvider>
+    </ColorModeContext.Provider>
   )
 }
 
